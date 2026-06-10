d631f48c7b62f190496a107e7bde9e71d8fa6dc0
Gonzalo <consor92@gmail.com>
2026-06-10 13:16:20 -0300

    Encriptacion

--- ManagerFile/src/main/java/GenericFiles/main.java
+++ ManagerFile/src/main/java/GenericFiles/main.java
@@ -1,7 +1,12 @@
 package GenericFiles;
 
 import java.io.PrintStream;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.ArrayList;
+
+import javax.crypto.SecretKey;
 
 public class main {
 
@@ -14,15 +19,30 @@ public class main {
 		// ps.println( mF.leerFileCaracterCaracter(mF.getFile()) );
 		// ps.println( mF.LeerFileConBuffer(mF.getFile()) );
 		// mF.modificarArchivoTemporalLinea(mF.getFile(), "Priner", "Primer");
+		/*
+		 * ArrayList<dtoProductos> lista = mF.archivoConLinkedList(mF.getFile());
+		 * 
+		 * for (dtoProductos l : lista) { ps.println("Nuevo Producto");
+		 * ps.println(String.format("%s %.2f %d", l.getProducto(), l.getPrecio(),
+		 * l.getCantidad() )); ps.println(String.join(" ", l.getProducto(),
+		 * String.valueOf(l.getPrecio()) , String.valueOf(l.getCantidad()) )); }
+		 */
 
-		ArrayList<dtoProductos> lista = mF.archivoConLinkedList(mF.getFile());
+		//Encriptado RSA priv-publ
+		String datoEncriptado = EncriptUtil.cifrarConClavePublica("Hola", (PublicKey)EncriptUtil.keys.get("RSAp"));
+		ps.printf("Texto a cifrar:%s \n ", "Hola");
+		ps.printf("Texto a Encriptado:%s \n Desencriptado:%s \n", datoEncriptado,
+				EncriptUtil.desCifrarConClavePublica(datoEncriptado, (PrivateKey)EncriptUtil.keys.get("RRSApr")));
 
-		for (dtoProductos l : lista) {
-			ps.println("Nuevo Producto");
-			ps.println(String.format("%s %.2f %d", l.getProducto(), l.getPrecio(), l.getCantidad() ));
-			ps.println(String.join(" ", l.getProducto(), String.valueOf(l.getPrecio()) , String.valueOf(l.getCantidad()) ));
-		}
-
+		//Encriptado AES  publico
+		SecretKey pKey = (SecretKey)EncriptUtil.keys.get("AES");
+		String dato = EncriptUtil.cifrarAES("Hola", pKey);
+		ps.println( dato );
+		ps.println( EncriptUtil.desCifrarAES(dato, pKey) );
+		
+		//HASH  SHA-256
+		EncriptUtil.hashSha("password");
+		
 	}
 
 }
